36b49886142c5626f8452a75cd5c219ab83561d8
giddy11 <edoghotugiddy@gmail.com>
2024-04-25 00:47:08 +0100

    implemented the dot operator in calculator

--- Assignment-6/Calculator-Project/script.js
+++ Assignment-6/Calculator-Project/script.js
@@ -20,6 +20,12 @@ const exponentBtn = document.getElementById('exponentBtn')
 const piBtn = document.getElementById('piBtn')
 const raiseToPowBtn = document.getElementById('raiseToPowBtn')
 const factorialBtn = document.getElementById('factorialBtn')
+const delBtn = document.getElementById('delBtn')
+const ceBtn = document.getElementById('ceBtn')
+const clearBtn = document.getElementById('clearBtn')
+const resultBox = document.getElementById('resultBox')
+
+
 
 
 
@@ -31,98 +37,103 @@ let multiplyBtnContent = document.getElementById('multiplyBtn').innerText
 // let symbols = ['+', '-', '*', '/'];
 let symbols = [plusBtnContent, minusBtnContent, divideBtnContent,  multiplyBtnContent];
 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
 
 
 let inputBox = document.getElementById('input-box')
+let inputBoxContent = document.getElementById('input-box').innerText
 
 oneBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '1';
-    } else {
-        inputBox.textContent += '1';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 1;
+    else
+        inputBox.textContent += '1';
 })
 
 twoBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '2';
-    } else {
-        inputBox.textContent += '2';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 2;
+    else
+        inputBox.textContent += '2';
 })
 
 threeBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '3';
-    } else {
-        inputBox.textContent += '3';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 3;
+    else
+        inputBox.textContent += '3';
 })
 
 fourBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '4';
-    } else {
-        inputBox.textContent += '4';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 4;
+    else
+        inputBox.textContent += '4';
 })
 
 fiveBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '5';
-    } else {
-        inputBox.textContent += '5';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 5;
+    else
+        inputBox.textContent += '5';
 })
 
 sixBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '6';
-    } else {
-        inputBox.textContent += '6';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 6;
+    else
+        inputBox.textContent += '6';
 })
 
 sevenBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '7';
-    } else {
-        inputBox.textContent += '7';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 7;
+    else
+        inputBox.textContent += '7';
 })
 
 eightBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '8';
-    } else {
-        inputBox.textContent += '8';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 9;
+    else
+        inputBox.textContent += '8';
 })
 
 nineBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '9';
-    } else {
+    let len = inputBox.textContent.length;
+
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 9;
+    else
         inputBox.textContent += '9';
-    }
 
 })
 
 zeroBtn.addEventListener('click', ()=>{
-    if (inputBox.textContent === '0') {
-        inputBox.textContent = '0';
-    } else {
-        inputBox.textContent += '0';
-    }
+    let len = inputBox.textContent.length;
 
+    if (inputBox.textContent == '0' || (inputBox.textContent[len - 1] == '0') && (!inputBox.textContent[len - 2] == '.'))
+        inputBox.textContent = inputBox.textContent.slice(0, len - 1) + 0;
+    else
+        inputBox.textContent += '0';
 })
 
 plusBtn.addEventListener('click', () => {
@@ -134,6 +145,25 @@ plusBtn.addEventListener('click', () => {
     else
         inputBox.textContent += plusBtnContent;
 })
+
+dotBtn.addEventListener('click', () => {
+    let inputText = inputBox.textContent;
+
+    let checkIndex = TheIndexOf(inputText, symbols)
+
+    if (checkIndex == -1){
+        inputText = newExpression(inputText)
+        console.log(inputText)
+    }
+    else{
+        let start = inputText.slice(0, checkIndex + 1)
+        let end = inputText.slice(checkIndex + 1)
+        inputText = start + newExpression(end)
+    }
+
+    inputBox.textContent = inputText
+})
+
 
 minusBtn.addEventListener('click', () => {
     let len = inputBox.textContent.length;
@@ -160,8 +190,45 @@ divideBtn.addEventListener('click', () => {
     let lastChar = inputBox.textContent[len - 1];
     console.log(divideBtnContent)
 
+    // this handles the case of // or */ or +-
     if (inputBox.textContent == '0' || symbols.includes(lastChar))
         inputBox.textContent = "0" + inputBox.textContent.slice(0, len - 1) + divideBtnContent;
     else
         inputBox.textContent += divideBtnContent;
 })
+
+ceBtn.addEventListener('click', ()=>{
+    let len = inputBoxContent.length
+    inputBox.textContent = inputBox.textContent.slice(0, len - 1)
+    resultBox.innerText = '0'
+
+})
+
+clearBtn.addEventListener('click', ()=>{
+    console.log(delBtn)
+    let len = inputBoxContent.length
+    inputBox.textContent = inputBox.textContent.slice(0, len - 1)
+})
+
+delBtn.addEventListener('click', ()=>{
+    let len = inputBoxContent.length
+    inputBox.textContent =  inputBox.textContent.slice(0, len - 2)
+})
+
+// Helper functions
+
+function TheIndexOf(expression, operators){
+
+    for (let i = 0; i < operators.length; i++) {
+        if (expression.includes(operators[i]))
+            return expression.indexOf(operators[i]);
+    }
+    return -1
+}
+
+function newExpression(expression){
+    if (expression.trim().length === 0 || !expression.includes('.'))
+        expression += "."
+
+    return expression
+}
